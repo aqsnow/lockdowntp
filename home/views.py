4f4523380481bb024bd3c8661524d91407289dbd
aqsnow <aqsnow@gmail.com>
2020-06-08 18:59:13 +0100

    test

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse("wrkds")
+    return render(request, "index.html") 
+
+def tasks(request):
+    #return HttpResponse("wrkds")
+    return render(request, "tasks.html")     
